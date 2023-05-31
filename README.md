66ea705d5c67afd5ee49567a925cd73779e3c9eb
p1k0pan <645758634@qq.com>
2023-05-31 22:27:20 +0200

    improve medium embeddings generate, update requirements, readme for embedding

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 send request like
 
 http://127.0.0.1:8000/test/search?query=how_to_cook_noodles
+
+## generate corpus embeddings
+
+if first time run
+
+```bash
+cd experiments
+python medium_embeddings.py --clean_corpus --gen_corpus_tensor
+```
--- experiments/medium_embeddings.py
+++ experiments/medium_embeddings.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 from sentence_transformers import SentenceTransformer, util
 import torch
@@ -6,7 +5,17 @@ import clean_dataset
 import json
 import time
 import datetime
-import os
+import argparse
+
+# Create the argument parser
+parser = argparse.ArgumentParser()
+
+# Add the arguments
+parser.add_argument('--clean_corpus', action='store_true', help='Flag to generate dataset')
+parser.add_argument('--gen_corpus_tensor', action='store_true', help='Flag to generate corpus tensor')
+
+# Parse the arguments
+args = parser.parse_args()
 
 
 if torch.cuda.is_available():
@@ -19,7 +28,7 @@ else:
 model = SentenceTransformer('paraphrase-MiniLM-L6-v2', device=device)
 
 def read_history() -> pd.DataFrame:
-    with open('/Users/piko/code/AdaptiveStoryFinder/experiments/food_health_data.json') as f:
+    with open('food_health_data.json') as f:
         data = json.load(f)
 
     history= []
@@ -30,21 +39,6 @@ def read_history() -> pd.DataFrame:
     # print(user_history.head())
     return user_history
 
-def read_dataset(path)-> pd.DataFrame:
-    df = pd.read_csv(path)
-    # Remove null description
-    df = df[~df.isna()]
-    # Renaming the description column
-
-    df.insert(2,'sentence','')
-    for _, row in df.iterrows():
-        row['sentence'] = str(row['title'])+": "+str(row['text'])
-        # row['sentence'] = row['article_name']+": "+row['article_desc']
-    # Sampling the first 5000 rows
-    # df = df.iloc[:5000]
-    # print(df.head())
-    return df
-
 def embed_text(text):
     return model.encode(text, convert_to_tensor=True,show_progress_bar=True)
 
@@ -54,8 +48,7 @@ def getTopResult(embedd1, embedd2, topk, df) -> pd.DataFrame:
     top_results = torch.topk(cos_scores, k=topk)
 
     print("\n\n======================\n\n")
-    # print("Query:", test_sentence)
-    print(f"\nTop {topk} most similar sentences in corpus:")
+    print(f"Top {topk} most similar sentences in corpus:")
 
     # result = top_results.indices.tolist()
     result_index = [] # get the top10 item from df
@@ -63,42 +56,56 @@ def getTopResult(embedd1, embedd2, topk, df) -> pd.DataFrame:
     for score, idx in zip(top_results[0], top_results[1]):
         score = score.cpu().data.numpy() 
         idx = idx.cpu().data.numpy()
-        print(f"{idx}: {df[['title']].iloc[idx]}")
+        print(f"{idx}: {df[['title']].iloc[idx].values}")
         result_index.append(idx)
         # result.append(df[['clean_sentence']].iloc[idx].item())
 
     return df.iloc[result_index].copy()
 
-def first_load():
-    dataset_path = 'medium_articles.csv'
-    print("read and clean data")
-    df: pd.DataFrame = read_dataset(dataset_path)
-    df = clean_dataset.clean_sentences(df)
-    df.to_csv('cleaned_meidium_articles.csv',',')
+def load_corpus():
+    if args.clean_corpus:
+        dataset_path = 'medium_articles.csv'
+        print("clean corpus dataset")
+        df = pd.read_csv(dataset_path)
+        df['sentence'] = df['title'] + ': ' + df['text']
+        df['sentence'] = df['sentence'].astype(str)
+        df = clean_dataset.clean_sentences(df)
+        df.to_csv('cleaned_medium_articles.csv',',')
+    else:
+        dataset_path = 'cleaned_medium_articles.csv'
+        print("load corpus dataset")
+        df = pd.read_csv(dataset_path)
+    return df
 
-    print("start embedding corpus")
-    corpus_embeddings = embed_text(df.clean_sentence.values)
-    print("end embedding corpus")
-    torch.save(corpus_embeddings, 'corpus_embeddings.pt')
-    print("saved")
+def load_corpus_tensor(df:pd.DataFrame):
+    if args.gen_corpus_tensor:
+        print("start embedding corpus")
+        corpus_embeddings = embed_text(df.clean_sentence.values)
+        print("end embedding corpus")
+        torch.save(corpus_embeddings, 'corpus_embeddings.pt')
+        print("saved")
+    else:
+        print("load corpus embedding")
+        corpus_embeddings = torch.load('corpus_embeddings.pt').to(device)
+    return corpus_embeddings
 
 if __name__ == "__main__":
     # user_history: pd.DataFrame = read_history()
-    # first_load
-    start_time = time.perf_counter()
-    first_load()
-    end_time = time.perf_counter()
-    elapsed_time_readable = str(datetime.timedelta(seconds=end_time - start_time))
-    print(elapsed_time_readable)
+    # load corpus df and embeddings
+    df = load_corpus()
+    # print(df[df['title'] == 'Hooked —']['url'])
+    corpus_embeddings = load_corpus_tensor(df)
 
-    # test_sentence = "start my own restaurant"
-    # query_embedding = embed_text(test_sentence)
+    test_sentence = "start my own restaurant"
+    print(test_sentence)
+    query_embedding = embed_text(test_sentence)
 
-    # # get result from corpus and query, then embedd the result 
-    # query_corpus_result:pd.DataFrame = getTopResult(query_embedding, corpus_embeddings, 10, df)
-    # query_corpus_result_embedding = embed_text(query_corpus_result.clean_sentence.values)
+    # get result from corpus and query, then embedd the result 
+    query_corpus_result:pd.DataFrame = getTopResult(query_embedding, corpus_embeddings, 10, df)
+    query_corpus_result_embedding = embed_text(query_corpus_result.clean_sentence.values)
 
     # # user history embedding
-    # user_keyword_embeddings = embed_text(user_history.clean_sentence.values)
+    user_history = read_history()
+    user_keyword_embeddings = embed_text(user_history.clean_sentence.values)
 
-    # top3 = getTopResult(user_keyword_embeddings, query_corpus_result_embedding, 3, query_corpus_result)
+    top3 = getTopResult(user_keyword_embeddings, query_corpus_result_embedding, 3, query_corpus_result)
