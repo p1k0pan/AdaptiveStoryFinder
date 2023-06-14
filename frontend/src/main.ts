6d3f2d21a3fdaf03cca98fb4ddeeb2d0a4abd8b8
Adrian <lindloff@uni-bremen.de>
2023-06-14 15:03:04 +0200

    bootstrap,primevue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,31 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import 'jquery/src/jquery.js';
+// import 'popper.js/dist/popper.min.js';
+
+import PrimeVue from 'primevue/config'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+// Vuetify
+Vue.use(PrimeVue)
+// PrimeVue
+Vue.use(PrimeVue)
+
+// theme
+// import 'primevue/resources/themes/lara-light-indigo/theme.css'
+// core
+// import 'primevue/resources/primevue.min.css'
+
 Vue.config.productionTip = false
 
 new Vue({
